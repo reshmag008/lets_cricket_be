a4a509083303b6c4daa827c3b13eab80f627acd2
Reshma <reshma.g@iinerds.com>
2024-02-05 20:27:49 +0530

    fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const { server } = require('./app');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 const startServer = async () => {
 
     server.listen(port, () => {
